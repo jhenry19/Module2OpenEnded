e6f2019f9ae62857333c59d55cdcd1c2bedc49a3
jhenry19 <jhny277@gmail.com>
2022-03-02 15:24:28 -0500

    Initial commit

---
+++ cmake-build-debug/CMakeFiles/Module2OpenEnded.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Module2OpenEnded.dir/main.cpp.o"
+  "Module2OpenEnded"
+  "Module2OpenEnded.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Module2OpenEnded.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Module2OpenEnded.dir/depend.make
@@ -0,0 +1,2 @@
+# Empty dependencies file for Module2OpenEnded.
+# This may be replaced when dependencies are built.
